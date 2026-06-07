b9cb4c53dffe948819e74de3a6589ca8bb73f8f8
1cos <massimiliano.zubboli@gmail.com>
2026-06-07 15:38:54 -0500

    Update push.js

--- js/push.js
+++ js/push.js
@@ -4,7 +4,7 @@ async function setupPush(){
   const reg=await navigator.serviceWorker.register('./sw.js');
   const p=await Notification.requestPermission();
   if(p!=='granted')return;
-  const key='BL2L-Q7TvbpWQbnUxt-n7MDB552hadn3Ggp2y1Cj8Nq6EVo0v4Fv2Ufy61mDgF6AFtK7NLV0QjwjndXXBpc';
+  const key='BKPndoZsWXaBIyAbk8fA_t_vD1pAdaTf-ii9gTqwYkPKDbEtKEvqOMTs4nWdxa7nREDxt_ld0IwAenLTIUC57zg';
   const applicationServerKey=Uint8Array.from(atob(key.replace(/-/g,'+').replace(/_/g,'/')),c=>c.charCodeAt(0));
   const sub=await reg.pushManager.getSubscription()||await reg.pushManager.subscribe({userVisibleOnly:true,applicationServerKey});
   await supa.from('push_subscriptions').upsert({user_name:user?.name||'Anonimo',subscription:sub.toJSON()},{onConflict:'user_name'});
