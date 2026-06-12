1e21909b7082e4af0471f6e508ef81cfac650500
1cos <82328630+1cos@users.noreply.github.com>
2026-06-11 21:45:57 -0500

    bump cache v45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'boh-v44';
+const CACHE_NAME = 'boh-v45';
 // ↑ Incrementa questo numero ad ogni deploy — es. v31, v32...
 // Il browser vede la versione diversa e aggiorna automaticamente
 
