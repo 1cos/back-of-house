670417c26647628bdc883674601de04bf45316ef
1cos <82328630+1cos@users.noreply.github.com>
2026-06-14 11:08:25 -0500

    v137: bump version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'boh-v136';
+const CACHE_NAME = 'boh-v137';
 // ↑ Incrementa questo numero ad ogni deploy — es. v31, v32...
 // Il browser vede la versione diversa e aggiorna automaticamente
 
