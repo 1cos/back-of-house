f762e773669fefed772daf0532ed373250002020
1cos <82328630+1cos@users.noreply.github.com>
2026-06-13 20:27:35 -0500

    v97: bump cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'boh-v96';
+const CACHE_NAME = 'boh-v95';
 // ↑ Incrementa questo numero ad ogni deploy — es. v31, v32...
 // Il browser vede la versione diversa e aggiorna automaticamente
 
