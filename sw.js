0622be8023e9d4ecada97e4a77fb4d8852e2c043
1cos <82328630+1cos@users.noreply.github.com>
2026-06-14 19:34:31 -0500

    bump: v154

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'boh-v153';
+const CACHE_NAME = 'boh-v154';
 // ↑ Incrementa questo numero ad ogni deploy — es. v31, v32...
 // Il browser vede la versione diversa e aggiorna automaticamente
 
