6849854bf62a3244d9e30a1a61512e3a51ab4dbb
1cos <82328630+1cos@users.noreply.github.com>
2026-06-16 10:42:57 -0500

    bump: boh-v203

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'boh-v202';
+const CACHE_NAME = 'boh-v203';
 // ↑ Incrementa questo numero ad ogni deploy — es. v31, v32...
 // Il browser vede la versione diversa e aggiorna automaticamente
 
